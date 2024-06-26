fb54dc68c53c31290e999fc5a446349e7015185e
Bruno Martins <bgcngm@gmail.com>
2024-06-26 21:31:36 +0200

    treewide: Split DTBs based on kernel config This is a workaround for the DTB[O]s merging issue that arises when adding all DTBs present in oplus dir. It could probably be solved with additional logic in Qualcomm's merge_dtbs script, but let's just keep it untouched.
    Change-Id: If6398639fb5e4f6238696691595516b6a8af4db3

--- qcom/camera/config/pineapple.mk
+++ qcom/camera/config/pineapple.mk
@@ -1,18 +1,22 @@
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
+
+ifeq ($(CONFIG_TARGET_IS_XIAOMI_DEVICE),y)
+dtbo-$(CONFIG_TARGET_PRODUCT_HOUJI) += houji-sm8650-camera-sensor.dtbo
+dtbo-$(CONFIG_TARGET_PRODUCT_SHENNONG) += shennong-sm8650-camera-sensor.dtbo
+dtbo-$(CONFIG_TARGET_PRODUCT_SUIREN) += suiren-sm8650-camera-sensor.dtbo
+dtbo-$(CONFIG_TARGET_PRODUCT_MANET) += manet-sm8650-camera-sensor.dtbo
+dtbo-$(CONFIG_TARGET_PRODUCT_AURORA) += aurora-sm8650-camera-sensor.dtbo
+dtbo-$(CONFIG_TARGET_PRODUCT_RUYI) += ruyi-sm8650-camera-sensor.dtbo
+dtbp-$(CONFIG_TARGET_PRODUCT_GOKU) += goku-sm8650-camera-sensor.dtbo
+else
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-cdp.dtbo \
 									pineapple-camera-sensor-mtp.dtbo \
 									pineapple-camera-sensor-hdk.dtbo \
 									pineapple-camera-sensor-qrd.dtbo \
-									shennong-sm8650-camera-sensor.dtbo \
-									manet-sm8650-camera-sensor.dtbo \
-									houji-sm8650-camera-sensor.dtbo  \
-									suiren-sm8650-camera-sensor.dtbo  \
-									aurora-sm8650-camera-sensor.dtbo \
-									ruyi-sm8650-camera-sensor.dtbo \
-									goku-sm8650-camera-sensor.dtbo \
 									pineapple-camera-sensor-qrd.dtbo \
 									pineapple-camera-sensor-aim500.dtbo
+endif
 
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
