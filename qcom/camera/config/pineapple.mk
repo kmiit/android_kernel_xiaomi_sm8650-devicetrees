4cc98a5b9a316b4feded91f5202a57aef27acf77
kmiit <kmiit@foxmail.com>
2025-10-18 17:25:02 +0800

    camera: Guard qcom default overlays
    Change-Id: I61d3d8e09b7c6c03c03eb8b4549e1b2aa5048289

--- qcom/camera/config/pineapple.mk
+++ qcom/camera/config/pineapple.mk
@@ -1,3 +1,5 @@
+ifneq ($(CONFIG_MI_HARDWARE_ID),)
+else
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-cdp.dtbo \
@@ -15,3 +17,4 @@ dtbo-$(CONFIG_ARCH_VOLCANO)     += volcano-camera-qrd.dtbo
 dtbo-$(CONFIG_ARCH_VOLCANO)     += volcano-camera-sensor-mtp.dtbo
 dtbo-$(CONFIG_ARCH_VOLCANO)     += volcano-camera-sensor-idp.dtbo
 dtbo-$(CONFIG_ARCH_VOLCANO)     += volcano-camera-sensor-qrd.dtbo
+endif
